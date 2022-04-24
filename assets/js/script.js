22ce420694ca3cf0bf63f67898ee7ac73dc31077
Tristin Nguyen <nguye501@msu.edu>
2022-04-23 21:37:01 -0400

    added variables for prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
 // Assignment code here
+function generatePassword(){
+
+  var possiblePw="";
+
+  var pwLength = window.prompt("How long do you want your generated password to be? (Must be between 8 and 128 characters)");
+
+
+
+
+  console.log(pwLength);
+
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+  var lowwer = "abcdefghijklmnopqrstuvxyz";
+  var numbers = "0123456789";
+  var specials = "!@#$%&*()+?";
+  
+
+
+
+}
+
 
 
 // Get references to the #generate element
